302a79873d060d5904f4697a2ffe7b9892357f7a
June Rademaker <june.rademaker@hva.nl>
2016-09-15 22:23:33 +0200

    Bacon points + stamina bar
    Bacon can now be collected. UI will show the amount of bacon collected.
    Stamina depletes over time. Stamina will be added when the player
    collects a bacon object.

--- Bacon Break/Assets/resources/Scripts/BaconScript.cs
+++ Bacon Break/Assets/resources/Scripts/BaconScript.cs
@@ -1,22 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BaconScript : MonoBehaviour {
 
-	// Use this for initialization
+    public Text txt_baconAmount;    //UI element displaying the amount of bacon collected.
+    public Image bar_stamina;       //UI element displaying the amount of stamina.
+
+    private int baconAmount;        //To keep track of the amount of collected bacon in code.
+
+	// Use this for initialization.
 	void Start () {
-	
+        baconAmount = 0;
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame.
 	void Update () {
 	
 	}
 
     void OnCollisionEnter(Collision col)
     {
+        //Add bacon points and stamina when the player collides with the bacon object.
+        //Destroy the bacon object.
         if (col.gameObject.tag == "Player")
         {
+            baconAmount++;
+            bar_stamina.GetComponent<StaminaScript>().AddStamina();
+            txt_baconAmount.text = baconAmount.ToString();
 
             Destroy(gameObject);
         }
--- Bacon Break/Assets/resources/Scripts/StaminaScript.cs
+++ Bacon Break/Assets/resources/Scripts/StaminaScript.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StaminaScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //Stamina decreases over time.
+        DrainStamina();
 	}
+
+    private void DrainStamina()
+    {
+        //Decrease fillAmount to simulate UI stamina drain.
+        gameObject.GetComponent<Image>().fillAmount -= Time.deltaTime / 50;
+    }
+
+    public void AddStamina()
+    {
+        //Increase fillAmount to simulate UI stamina gain.
+        //(By collecting bacon objects, see "BaconScript").
+        gameObject.GetComponent<Image>().fillAmount += 0.2f;
+    }
 }
