37471e7adcb5a4fb79c60dc97277d82c3c8ed2d3
Manuel Martin <man.ual@hotmail.com>
2016-09-19 14:52:26 +0200

    New traps! - rolling rockss - wall with spikes - presure plates - loose sawblades

--- Bacon Break/Assets/resources/Scripts/TrapTap.cs
+++ Bacon Break/Assets/resources/Scripts/TrapTap.cs
@@ -1,19 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TrapTap : MonoBehaviour {
+public class TrapTap : MonoBehaviour
+{
+    public bool movementStoppable = false;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public bool canUnleash = false;
+    bool unleash;
+
+    Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unleash)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 5, Space.World);
+        }
+    }
     void OnMouseDown()
     {
-        Destroy(this.gameObject);
+        if (!movementStoppable && !canUnleash)
+            Destroy(this.gameObject);
+
+        if (movementStoppable)
+            rb.isKinematic = true;
+
+        if (canUnleash)
+        {
+            unleash = true;
+        }
+    }
+    void OnMouseUp()
+    {
+        if (movementStoppable)
+            rb.isKinematic = false;
+    }
+    void OnMouseExit()
+    {
+        if (movementStoppable)
+            rb.isKinematic = false;
     }
 }
