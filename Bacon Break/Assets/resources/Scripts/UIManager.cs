cf0f9c06807e7cd461f59ce9eb7c66c947342abc
Dylan Gijsbertsen Laptop <d.b.gijsbertsen@live.nl>
2016-09-19 12:12:32 +0200

    Added main menu to the project

---
+++ Bacon Break/Assets/resources/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// For now play the level we have created.
+    /// TODO:: go to select screen instead of play a level.
+    /// </summary>
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    /// <summary>
+    /// Quit the game
+    /// </summary>
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
