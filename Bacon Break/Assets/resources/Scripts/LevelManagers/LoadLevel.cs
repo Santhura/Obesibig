0bf88fd5fd95938d0de4621c53f467c5e54a45c8
Manuel Martin <man.ual@hotmail.com>
2016-09-26 12:55:23 +0200

    The new tutorial level as well as the level selector have been added. along with this is an asset of simplistic icons.

---
+++ Bacon Break/Assets/resources/Scripts/LevelManagers/LoadLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+    public string thisLevel;
+    GameObject loadLevel;
+	// Use this for initialization
+	void Start () {
+        thisLevel = PlayerPrefs.GetString("level");
+        LoadMyLevel();
+      
+    }
+    void LoadMyLevel()
+    {
+        loadLevel = (GameObject)Instantiate(Resources.Load("Prefabs/Levels/" + thisLevel));
+    }
+	
+}
