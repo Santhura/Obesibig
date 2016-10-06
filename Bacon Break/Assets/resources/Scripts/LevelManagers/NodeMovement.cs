45b4427434e76cc3b79886fb5308187efe6bb0dc
June Rademaker <june.rademaker@hva.nl>
2016-10-06 23:01:17 +0200

    Progress.
    Sorry hoor heb even geen zin in een lange update. Dat komt morgen wel.

--- Bacon Break/Assets/resources/Scripts/LevelManagers/NodeMovement.cs
+++ Bacon Break/Assets/resources/Scripts/LevelManagers/NodeMovement.cs
@@ -1,31 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*This class is used for moving a character to a levelnode.
+ * When the player taps a node, the character will move to it from the node it's currently positioned at.
+ * TODO:: Popup with play button, level prefab added to node so it can be loaded.
+ */
+
 public class NodeMovement : MonoBehaviour
 {
-    public Vector3[] nodes;
-    private Vector3[] paths;
+    //Pubs
+    public Vector3[] nodes;                     //Masterlist of iTween nodes
+
+    //Privates ( ;) )
+    private Vector3[] subPath;                  //Sub-path coordinates, used for character movement between level nodes
+    private GameObject player;                  //The player, obviously
+    private int startIndex, endIndex;           //The start index and the end index for the sub path array
+    private bool isMoving;                      //Boolean which checks if the character is moving from one node to another
 
     // Use this for initialization
     void Start()
     {
+        //Start index of the iTween path.
+        startIndex = 0;
+
+        player = GameObject.FindWithTag("Player");
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Space))
+        //FOR COMPUTER DEBUG PURPOSES, DO NOT REMOVE.
+        /*if (Input.GetKeyUp(KeyCode.Space))
         {
-            Vector3[] path = new Vector3[2];
+            Debug.Log(startIndex);
+            endIndex = 2;
+
+            Vector3[] path = new Vector3[50];
             path = GetPath(0, 2);
 
-            iTween.MoveTo(gameObject, iTween.Hash("position", path[path.Length-1], "time", 10, "easetype", iTween.EaseType.easeInOutSine));
+            iTween.MoveTo(player.gameObject, iTween.Hash("path", path, "time", 10, "easetype", iTween.EaseType.easeInOutSine));
+            isMoving = true;
+        }*/
+
+        //Move the player to the requested location.
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            //Can only start movement when the character isn't already moving.
+            if (Input.GetTouch(i).phase == TouchPhase.Began && !isMoving)
+            {
+                //Construct a ray from the current touch coordinates.
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                RaycastHit hit;
+
+                //Check if node is tapped and move the character to that node.
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    if (hit.collider.tag == "Node")
+                    {
+                        //Use start and end index to get a sub-path the character can traverse.
+                        GameObject levelNode = hit.transform.gameObject;
+                        endIndex = int.Parse(levelNode.name.Substring(levelNode.name.Length - 2)) * 2;
+
+                        Vector3[] path = new Vector3[Mathf.Abs(endIndex - startIndex) + 1];
+                        path = GetPath(startIndex, endIndex);
+                        
+                        //Move the object to the specified location using the sub-path.
+                        iTween.MoveTo(player.gameObject, iTween.Hash("path", path, "time", 10, "easetype", iTween.EaseType.easeInOutSine));
+
+                        //Character is on the move.
+                        isMoving = true;
+                    }
+                }
+            }
+
+            //Check if the character has reached the specified node.
+            if (isMoving)
+            {
+                if (Mathf.Abs(Vector3.Distance(player.transform.position, nodes[endIndex])) <= 0.5f)
+                {
+                    startIndex = endIndex;
+                    isMoving = false;
+                }
+            }
         }
     }
 
     Vector3[] GetPath(int startIndex, int endIndex)
     {
-        paths = new Vector3[Mathf.Abs(endIndex - startIndex) + 1];
+        subPath = new Vector3[Mathf.Abs(endIndex - startIndex) + 1];
 
         //Move path in opposite direction if startIndex is greater than endIndex.
         //Negative or positive value...
@@ -39,14 +101,12 @@ public class NodeMovement : MonoBehaviour
             sign = 1;
         }
 
-        //Assign values from nodes to path
-        for (int i = 0; i < paths.Length; i++)
+        //Assign requested values from nodes to path.
+        for (int i = 0; i < subPath.Length; i++)
         {
-            Debug.Log("Hoi");
-            paths[i] = nodes[startIndex + (sign * i)];
-            //paths[i] = nodes[i];
+            subPath[i] = nodes[startIndex + (sign * i)];
         }
 
-        return paths;
+        return subPath;
     }
 }
