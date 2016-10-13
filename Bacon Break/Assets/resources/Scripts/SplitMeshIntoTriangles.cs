5fb912a6a06a0f0bdfa0e7c1fa1239579be00cb2
Dylan Gijsbertsen Laptop <d.b.gijsbertsen@live.nl>
2016-10-13 10:20:54 +0200

    Renamed some variables in split mesh script

--- Bacon Break/Assets/resources/Scripts/SplitMeshIntoTriangles.cs
+++ Bacon Break/Assets/resources/Scripts/SplitMeshIntoTriangles.cs
@@ -8,15 +8,15 @@ public class SplitMeshIntoTriangles : MonoBehaviour {
 
     IEnumerator SplitMesh() {
         // get all the meshen vertices/ normals and uvs
-        SkinnedMeshRenderer smr = GetComponentInChildren<SkinnedMeshRenderer>();
-        MeshFilter mf = GetComponent<MeshFilter>();
-        Mesh m = mf.mesh;
-        Vector3[] verts = m.vertices;
-        Vector3[] normls = m.normals;
-        Vector2[] uvs = m.uv;
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = meshFilter.mesh;
+        Vector3[] verts = mesh.vertices;
+        Vector3[] normls = mesh.normals;
+        Vector2[] uvs = mesh.uv;
 
-        for (int submesh = 0; submesh < m.subMeshCount; submesh++) {
-            int[] indices = m.GetTriangles(submesh);
+        for (int submesh = 0; submesh < mesh.subMeshCount; submesh++) {
+            int[] indices = mesh.GetTriangles(submesh);
             for (int i = 0; i < indices.Length / 4.25f; i += 3) {
                 Vector3[] newVerts = new Vector3[3];
                 Vector3[] newNormls = new Vector3[3];
@@ -30,12 +30,12 @@ public class SplitMeshIntoTriangles : MonoBehaviour {
                     newNormls[n] = normls[index];
                 }
                 // create a new mesh with the new vertices/normals and uvs
-                Mesh mesh = new Mesh();
-                mesh.vertices = newVerts;
-                mesh.normals = newNormls;
-                mesh.uv = newUvs;
+                Mesh newMesh = new Mesh();
+                newMesh.vertices = newVerts;
+                newMesh.normals = newNormls;
+                newMesh.uv = newUvs;
 
-                mesh.triangles = new int[] {0,1,2,
+                newMesh.triangles = new int[] {0,1,2,
                                             2,1,0};
                 
                 // create trianle set position and rotation to the original position of the player
@@ -45,14 +45,14 @@ public class SplitMeshIntoTriangles : MonoBehaviour {
                 go.transform.rotation = transform.rotation;
                 go.transform.rotation = new Quaternion(transform.rotation.x, 180, transform.rotation.z, 0);
                 go.transform.localScale = new Vector3(3, 3, 3);
-                go.AddComponent<MeshRenderer>().material = smr.materials[submesh];
-                go.AddComponent<MeshFilter>().mesh = mesh;
+                go.AddComponent<MeshRenderer>().material = skinnedMeshRenderer.materials[submesh];
+                go.AddComponent<MeshFilter>().mesh = newMesh;
                 go.AddComponent<BoxCollider>();
                 go.AddComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRadius);
                 Destroy(go, 3 + (Random.Range(0.0f, 0.5f)));
             }
         }
-        smr.enabled = false;
+        skinnedMeshRenderer.enabled = false;
         GameObject.Find("Main Camera").transform.parent = null;
         Time.timeScale = 0.5f;
         Destroy(gameObject);
