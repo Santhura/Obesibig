ccc18a4f37ecdf780927a79e06b7bb5f72ca8c0b
Nirish Alakhramsing <nirish.alakhramsing@hva.nl>
2016-10-14 09:51:06 +0200

    Added splashscreen to the main menu for our teamlogo

---
+++ Bacon Break/Assets/resources/Scripts/SplashScreenScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SplashScreenScript : MonoBehaviour {
+
+    public Button[] disableMMbuttons;
+    private GameObject splashPanel;
+
+    void Awake () {
+
+        splashPanel = GameObject.Find("SplashPanel");
+
+        for (int i = 0; i < disableMMbuttons.Length; i++)
+        {
+            disableMMbuttons[i].interactable = false;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Waitforseconds());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Waitforseconds() {
+
+        yield return new WaitForSeconds(3);
+        splashPanel.SetActive(false);
+
+        for (int i = 0; i < disableMMbuttons.Length; i++)
+        {
+            disableMMbuttons[i].interactable = true;
+        }
+
+
+    }
+}
