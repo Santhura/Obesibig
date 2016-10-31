c42543953fa38afe8fd9a7adae97f309eb17840c
June Rademaker <june.rademaker@hva.nl>
2016-10-31 11:59:57 +0100

    Projectmappen opgeruimt

--- Bacon Break/Assets/Scripts/Traps/TrapHitbox.cs
+++ Bacon Break/Assets/Scripts/Traps/TrapHitbox.cs
@@ -2,19 +2,18 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class TrapHitbox : MonoBehaviour {
+public class TrapHitbox : MonoBehaviour
+{
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            //Destroy(other.gameObject);
-            //SceneManager.LoadScene(0);
-            CameraShake.isHit = true;
-            for (int i = 0; i < GetComponents<Collider>().Length; i++) {
+            for (int i = 0; i < GetComponents<Collider>().Length; i++)
+            {
                 GetComponents<Collider>()[i].enabled = false;
             }
+
             PlayerMovement.isAbleToMove = false;
-            //CameraShake.deathParticle.SetActive(true);
             WinOrLoseScript.isDead = true;
         }
     }
