c992a146285365ac7890f5218a168837e0c8ab6b
Manuel Martin <man.ual@hotmail.com>
2016-10-31 12:22:33 +0100

    Tweak traps + moving sawblade & blade trap interactable

---
+++ Bacon Break/Assets/resources/Scripts/Traps/StopTrapAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopTrapAnimation : MonoBehaviour
+{
+    public Animation trapAnimation;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        trapAnimation.enabled = false;
+    }
+}
