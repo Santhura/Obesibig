53393930e73d4937f2547145a01d8a02e7e4de98
June Rademaker <june.rademaker@hva.nl>
2016-11-01 22:53:41 +0100

    Open en close shop

--- Bacon Break/Assets/Scripts/Shop/ShopController.cs
+++ Bacon Break/Assets/Scripts/Shop/ShopController.cs
@@ -6,4 +6,48 @@ public class ShopController : MonoBehaviour
 {
     //Keeps track of all the shop items.
     public List<ShopItem> shopItems;
+
+    //The actual shop
+    public GameObject shopContainer;
+
+    //For opening and closing the shop
+    private bool shopOpened;
+
+    void Start()
+    {
+        if (shopContainer.activeSelf)
+        {
+            shopOpened = true;
+        }
+        else
+        {
+            shopOpened = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Q) && !shopOpened)
+        {
+            OpenShop();
+        }
+        else if (Input.GetKeyUp(KeyCode.Q) && shopOpened)
+        {
+            CloseShop();
+        }
+    }
+
+    void OpenShop()
+    {
+        shopContainer.SetActive(true);
+        Time.timeScale = 0;
+        shopOpened = true;
+    }
+
+    void CloseShop()
+    {
+        shopContainer.SetActive(false);
+        Time.timeScale = 1;
+        shopOpened = false;
+    }
 }
