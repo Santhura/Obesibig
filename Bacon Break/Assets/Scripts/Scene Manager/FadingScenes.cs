95a71389db6949404ff84c51361bba12349f7c60
Dylan Gijsbertsen Laptop <d.b.gijsbertsen@live.nl>
2016-11-01 11:58:05 +0100

    Added fading from splash screen

---
+++ Bacon Break/Assets/Scripts/Scene Manager/FadingScenes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadingScenes : MonoBehaviour {
+
+    public Image fadeOutImage;               // the texture that will overlay the screen. This can be a black iamge or a loading graphic
+    public float fadeSpeed = 3f;             // the fading speed
+
+    private float alpha = 1.0f;              // the texture's alpha value between 0 and 1
+    private float fadeDir = 1;               // the direction to fade : in = -1 or out = 1
+
+    public static bool activateFade;        // Activefade when the fading has to start
+    private Color fadeColor;                // color that will be set for the image
+
+	// Use this for initialization
+	void Start () {
+        fadeOutImage = GameObject.Find("Fade").GetComponent<Image>();
+        activateFade = false;
+        fadeColor = new Color(0, 0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (activateFade) {
+            fadeColor.a += fadeSpeed * fadeDir * Time.deltaTime;
+            fadeOutImage.color = fadeColor;
+            if(fadeColor.a >= 1) {
+                GameManager.SwitchScene("Main Menu", null);
+            }
+        }
+	}
+}
--- Bacon Break/Assets/Scripts/SplashScreenScript.cs
+++ Bacon Break/Assets/Scripts/SplashScreenScript.cs
@@ -8,6 +8,6 @@ public class SplashScreenScript : MonoBehaviour {
     //Shows splash screen for couple seconds
 	IEnumerator Start () {
         yield return new WaitForSeconds(2);
-        GameManager.SwitchScene("Main Menu", null);
+        FadingScenes.activateFade = true;
     }
 }
