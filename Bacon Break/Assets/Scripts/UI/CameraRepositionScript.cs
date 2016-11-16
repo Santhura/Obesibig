946e4a26cbc2dead45e6f90349d7a1994187eb36
Nirish Alakhramsing <nirish.alakhramsing@hva.nl>
2016-11-17 00:07:23 +0100

    Working on run-time Camera editing and saving system for future efficient editing

--- Bacon Break/Assets/Scripts/UI/CameraRepositionScript.cs
+++ Bacon Break/Assets/Scripts/UI/CameraRepositionScript.cs
@@ -4,48 +4,127 @@ using System.Collections;
 public class CameraRepositionScript : MonoBehaviour {
 
     public bool tunnelLv8 = false;
-    private Transform cameraOldtransform;
+    private float originalSpeed;
+    public float originalRotX, originalRotY, originalRotZ;
     private Transform cameraNewtransform;
+    private Transform cameraManipulator;
+    public Transform lookAtPlayer;
     public GameObject getCamera;
+    public GameObject playerObj;
+
+    public float manipulatorSpeed;
 
 	// Use this for initialization
 	void Start () {
         getCamera = GameObject.Find("Main Camera");
-	}
+        cameraManipulator = getCamera.transform;
+
+        lookAtPlayer = GameObject.Find("Player").transform;
+        playerObj = GameObject.Find("Player");
+
+        //  Set original position and rotation
+        originalRotX = transform.rotation.x;
+        originalRotY = transform.rotation.y;
+        originalRotZ = transform.rotation.z;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-        if (tunnelLv8)
+
+        //transform.LookAt(lookAtPlayer);
+
+        //***** Turn on CAMERA EDITING MODE ****
+
+        //  Show warning text "Camera editing!"
+        //  Show controls for moving the camera
+        //  Show how to turn exit mode.
+        //  Show how to save positions.
+
+        //  Show reset position or rotation
+
+        //  Set run speed player to zero or make him run again.
+        if (Input.GetKeyDown(KeyCode.L) && playerObj.GetComponent<PlayerMovement>().baseSpeed > 0)
         {
-            cameraNewtransform = GameObject.Find("TunnelPosition").transform;
+            originalSpeed = playerObj.GetComponent<PlayerMovement>().baseSpeed;
+            playerObj.GetComponent<PlayerMovement>().baseSpeed = 0;
+        }   else if (Input.GetKeyDown(KeyCode.K) && playerObj.GetComponent<PlayerMovement>().baseSpeed == 0)
+            {
+                playerObj.GetComponent<PlayerMovement>().baseSpeed = originalSpeed;
+                originalSpeed = 0;
+            }
 
-            iTween.MoveTo(getCamera, iTween.Hash("position", cameraNewtransform.position, "time", 1f, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.none));
-            //getCamera.transform.position = cameraNewtransform.position;
-            iTween.RotateTo(getCamera, iTween.Hash("x", 30f, "time", 1f, "islocal", true, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.none));
-            //getCamera.transform.rotation = cameraNewtransform.rotation;
-
-            tunnelLv8 = false;
-        }
-	}
-
-    void OnTriggerEnter(Collider col)
-    {
-        if(gameObject.name == "ArrowTrigger1")
-        { 
-            tunnelLv8 = true;
-        }
-
-        if (gameObject.name == "TriggerCamereOldPos")
+        //  Set original rotation
+        if (Input.GetKeyDown(KeyCode.O))
         {
-            Debug.Log("Old CAMERA Trigger");
-            cameraOldtransform = GameObject.Find("StandardCameraPosition").transform;
-            iTween.MoveTo(getCamera, iTween.Hash("position", cameraOldtransform.position, "time", 1f, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.none));
-            //getCamera.transform.position = cameraOldtransform.position;
-            iTween.RotateTo(getCamera, iTween.Hash("x", 35.4f, "time", 1f, "islocal", true, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.none));
-            //getCamera.transform.rotation = cameraOldtransform.rotation;
+            Quaternion.Euler(originalRotX, originalRotY, originalRotZ);
         }
-    }
 
+        //  Set new position
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log(originalRotX);
+            Debug.Log(originalRotY);
+            Debug.Log(originalRotZ);
+            // Save position in a list position.
+
+        }
+
+        //  Movement Camera controls
+
+
+
+        //Strafe left or right
+        if (Input.GetKey(KeyCode.Keypad7))
+        {
+            Debug.Log("Pressed 4 numeric key");
+            gameObject.transform.Translate(Vector3.left * manipulatorSpeed * Time.deltaTime);
+        }   else if (Input.GetKey(KeyCode.Keypad9))
+            {
+                Debug.Log("Pressed 6 numeric key");
+                gameObject.transform.Translate(Vector3.right * manipulatorSpeed * Time.deltaTime);
+            }
+
+        //Go up or down
+        if (Input.GetKey(KeyCode.KeypadDivide))
+        {
+            Debug.Log("Pressed 8 numeric key");
+            getCamera.transform.Translate(Vector3.up * manipulatorSpeed * Time.deltaTime);
+        }   else if (Input.GetKey(KeyCode.Keypad8))
+            {
+                Debug.Log("Pressed 2 numeric key");
+            getCamera.transform.Translate(Vector3.down * manipulatorSpeed * Time.deltaTime);
+            }
+
+        //Rotate to the left or right
+        if (Input.GetKey(KeyCode.Keypad5))
+        {
+            Debug.Log("Pressed 8 numeric key");
+            getCamera.transform.Rotate(Vector3.left * manipulatorSpeed * Time.deltaTime);
+        }   else if (Input.GetKey(KeyCode.Keypad2))
+            {
+                Debug.Log("Pressed 2 numeric key");
+                getCamera.transform.Rotate(Vector3.right * manipulatorSpeed * Time.deltaTime);
+            }
+
+        //Rotate upwards or dowards
+        if (Input.GetKey(KeyCode.Keypad1))
+        {
+            Debug.Log("Pressed 8 numeric key");
+            getCamera.transform.Rotate(Vector3.up * manipulatorSpeed * Time.deltaTime);
+        }   else if (Input.GetKey(KeyCode.Keypad3))
+            {
+                Debug.Log("Pressed 2 numeric key");
+                getCamera.transform.Rotate(Vector3.down * manipulatorSpeed * Time.deltaTime);
+            }   else if (Input.GetKey(KeyCode.Keypad4))
+                {
+                    Debug.Log("Pressed 2 numeric key");
+                    getCamera.transform.Rotate(0, 0, manipulatorSpeed * Time.deltaTime);
+                }   else if (Input.GetKey(KeyCode.Keypad6))
+                    {
+                        Debug.Log("Pressed 2 numeric key");
+                        getCamera.transform.Rotate(0, 0, -manipulatorSpeed * Time.deltaTime);
+                    }
+
+    }   
 
 }
