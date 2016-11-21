8c9484d36979595757b5064d9dae6069d686ec0e
Dylan Gijsbertsen Laptop <d.b.gijsbertsen@live.nl>
2016-11-21 14:26:52 +0100

    added swipe boost, swipe with the lane and you will boost. 1 thing is that you will also boost if you want to move but swipe some bit on the horizontal as

---
+++ Bacon Break/Assets/Scripts/Player/SwipeBooster.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeBooster : MonoBehaviour {
+
+    private Vector3 firstTouch, secondTouch, distance;
+    private bool isSwiped;
+    public float sensitivity = .2f;
+	// Use this for initialization
+	void Start () {
+        isSwiped = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Touch touch in Input.touches) {
+            if(touch.phase == TouchPhase.Began) {
+                firstTouch = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0));
+            }
+
+            if(touch.phase == TouchPhase.Moved && !isSwiped) {
+                secondTouch = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0 ));
+
+                float deltaX = secondTouch.z - firstTouch.z;
+                float deltaY = secondTouch.y - firstTouch.y;
+
+                distance = new Vector3(0, deltaY, deltaX);
+                bool swipedSideways = Mathf.Abs(deltaX) > Mathf.Abs(deltaY);
+
+                if(distance != null) {
+                    if(/*swipedSideways && */distance.z > sensitivity) {
+                        StaminaScript.isBoosting = true; 
+                    }
+                    else if(/*swipedSideways && */distance.z < -sensitivity) {
+                        StaminaScript.isBoosting = false;
+                    }
+                }
+            }
+        }
+	}
+}
