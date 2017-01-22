fdff54e82d5233862f52c27dc660297a4b9f51d9
June Rademaker <june.rademaker@hva.nl>
2017-01-22 20:43:02 +0100

    Some minor changes
    Comments, debugs etc.

--- Bacon Break/Assets/Scripts/Shop/InventoryController.cs
+++ Bacon Break/Assets/Scripts/Shop/InventoryController.cs
@@ -168,7 +168,6 @@ public class InventoryController : MonoBehaviour
     {
         if (itemType == "character")
         {
-            Debug.Log(characterItems[index].itemName);
             characterTitle.text = characterItems[index].itemName;
             characterImage.sprite = characterItems[index].itemSprite;
             characterDescription.text = characterItems[index].itemDesc;
